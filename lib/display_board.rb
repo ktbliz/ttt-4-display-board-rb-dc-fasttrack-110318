50b3928bb2c0eab7cea7e2d8ee4281e1cd7f4b68
Katie Blizzard <kblizzard11@gmail.com>
2018-11-03 18:34:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,6 +7,5 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
  puts "-----------"
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
- display_board
  display_board(["H", "E", "Y", "B", "B", " ", "L", "U", "V"])
  
